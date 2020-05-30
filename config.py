8fc70de3b0d2937b639671c2d3067379f7814c3d
paulmburu08 <paulmburu08@gmail.com>
2020-05-31 00:04:48 +0300

    app __init__.py content added

--- config.py
+++ config.py
@@ -2,7 +2,20 @@ import os
 
 class Config:
     TOP_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?country={}&apiKey={}'
+    SEARCH_HEADLINES = 'https://newsapi.org/v2/top-headlines?q={}&apiKey={}'
     CATEGORY_URL = 'https://newsapi.org/v2/top-headlines?country={}&category={}&apiKey={}'
     ARTICLES_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+    'development' : DevConfig,
+    'production' : ProdConfig 
+}
+
