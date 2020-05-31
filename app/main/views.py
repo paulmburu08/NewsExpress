99b24f3ef219f48e84cfc22203f3adc327c14ff2
paulmburu08 <paulmburu08@gmail.com>
2020-05-31 21:34:08 +0300

    add navbar

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from . import main
 from flask import render_template
-from ..requests import get_headlines
+from ..requests import get_headlines,get_category
 
 @main.route('/')
 def index():
@@ -17,3 +17,19 @@ def index():
     print(us_news)
     title = 'Welcome to newsExpress'
     return render_template('index.html', title = title, usheadlines = us_news, edheadlines = german_news, frheadlines = french_news, ruheadlines = russia_news, saheadlines = saudi_news)
+
+@main.route('/category')
+def category():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    business = get_category('us','business')
+    entertainment = get_category('us','entertainment')
+    health = get_category('us','health')
+    science = get_category('us','science')
+    sports = get_category('us','sports')
+    technology = get_category('us','technology')
+
+    return render_template('navbar.html',business = business, entertainment = entertainment, health = health, science = science, sports = sports, technology = technology)
+
--- app/requests.py
+++ app/requests.py
@@ -3,11 +3,17 @@ from .models import TopHeadline
 
 api_key = None
 headlines_url = None
+search_headlines = None
+category_url = None
+articles_url = None
 
 def configure_request(app):
-    global api_key,headlines_url
+    global api_key,headlines_url,search_headlines,category_url,articles_url
     api_key = app.config['NEWS_API_KEY']
     headlines_url = app.config['TOP_HEADLINES_URL']
+    search_headlines = app.config['SEARCH_HEADLINES']
+    category_url = app.config['CATEGORY_URL']
+    articles_url = app.config['ARTICLES_URL']
 
 def get_headlines(country):
     '''
@@ -45,5 +51,23 @@ def process_results(headlines_list):
 
     return headlines_result
     
+def get_category(country,category):
+    '''
+    Function that gets the json response to our url request
+    '''
+
+    get_category_url = category_url.format(country,category,api_key)
+
+    with urllib.request.urlopen(get_category_url) as url:
+        get_category_data = url.read()
+        get_category_response = json.loads(get_category_data)
+
+        category_results = None
+
+        if get_category_response['articles']:
+            category_list = get_category_response['articles']
+            category_results = process_results(category_list)
+
+    return category_results
 
     
