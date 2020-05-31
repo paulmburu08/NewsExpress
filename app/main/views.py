aa9915496c03d17143bb2cf4fdfe22a08b91a8f4
paulmburu08 <paulmburu08@gmail.com>
2020-05-31 14:59:39 +0300

    add headlines route to views

---
+++ app/main/views.py
@@ -0,0 +1,15 @@
+from . import main
+from flask import render_template
+from ..requests import get_headlines
+
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    #getting news headlines
+    us_news = get_headlines('us')
+    print(us_news)
+    title = 'Welcome to newsExpress'
+    return render_template('index.html' title=title, headlines = us_news)
