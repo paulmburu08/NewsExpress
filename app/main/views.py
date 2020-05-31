9feed72e6591e84857990e6dcb198f7028624889
paulmburu08 <paulmburu08@gmail.com>
2020-05-31 18:17:58 +0300

    add macros

--- app/main/views.py
+++ app/main/views.py
@@ -9,7 +9,11 @@ def index():
     '''
 
     #getting news headlines
-    us_news = get_headlines('in')
+    us_news = get_headlines('us')
+    german_news = get_headlines('ed')
+    french_news = get_headlines('fr')
+    russia_news = get_headlines('ru')
+    saudi_news = get_headlines('sa')
     print(us_news)
     title = 'Welcome to newsExpress'
-    return render_template('index.html', title = title, headlines = us_news)
+    return render_template('index.html', title = title, usheadlines = us_news, edheadlines = german_news, frheadlines = french_news, ruheadlines = russia_news, saheadlines = saudi_news)
